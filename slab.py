7b54120f81d9339953a53497f16f121663f37f2f
Tae Yun Kim <kimtaeyun@outlook.com>
2020-04-28 16:12:53 +0900

    more cleaning, setting offset of the window

--- slab.py
+++ slab.py
@@ -18,7 +18,7 @@ atomsymtonum = {
     }
 
 
-def celldmtocell(celldm):
+def celldm2cell(celldm):
     A, B, C = celldm[:3]
     degtorad = np.pi/180.0
     alpha, beta, gamma = [x*degtorad for x in celldm[3:]]
@@ -36,7 +36,7 @@ def celldmtocell(celldm):
     return a
 
 
-def celltocelldm(s):
+def cell2celldm(s):
     A, B, C = [np.linalg.norm(s[i, :]) for i in range(3)]
 
     cosbc = np.dot(s[1, :]/B, s[2, :]/C)
@@ -52,7 +52,12 @@ def celltocelldm(s):
     return [A, B, C, alpha, beta, gamma]
 
 
-def getreciprocal(a):
+def standardizeCell(a):
+    celldm = cell2celldm(a)
+    return celldm2cell(celldm)
+
+
+def getReciprocal(a):
     a = np.array(a)
     V = np.linalg.det(a)
     if V < 0: raise ValueError("det(a) < 0")
@@ -65,7 +70,14 @@ def getreciprocal(a):
     return b/V
 
 
-def readvestaxtl(fpath):
+def getGvector(a, g):
+    a = np.array(a).astype(float)
+    g = np.array(g).astype(int)
+    b = getReciprocal(a)
+    return np.dot(b, g)
+
+
+def readVestaxtl(fpath):
     with open(fpath) as f:
         if f.readline().split()[0] != "TITLE":
             raise ValueError
@@ -89,7 +101,7 @@ def readvestaxtl(fpath):
         atompos = np.array(atompos)
         atompos = np.mod(atompos, [1.0, 1.0, 1.0])
 
-    cell = celldmtocell(celldm)
+    cell = celldm2cell(celldm)
     out = {}
     out["cell_parameters"] = {"option": "angstrom", "cell": cell}
     out["atomic_positions"] = {"option": "crystal", "position": atompos, "label": atomlabel}
@@ -97,14 +109,16 @@ def readvestaxtl(fpath):
     return out
 
 
-def writexsf(f, a, c, l, v, mode="crystal"):
+def writexsf(f, a, l, v, mode="crystal"):
     """
-        write structure in xsf format
-        a: primitive cell
-        c: conventional cell
-        l: atom labels
-        v: atomic coordinates within primitive cell
-        mode: "crystal", "slab", "polymer", "dot"
+    Args:
+        f: File object
+        a: Primitive cell
+        l: Atom label
+        v: Atomic coordinates
+        mode: "crystal", "slab", "polymer", "dot". Default to "crystal"
+
+    See also:
         all coordinates in cartesian coordinate
     """
     f.write(f"{mode.upper():s}\n")
@@ -117,7 +131,7 @@ def writexsf(f, a, c, l, v, mode="crystal"):
     f.write("CONVVEC\n")
 
     for i in range(3):
-        x, y, z = c[i, :]
+        x, y, z = a[i, :]
         f.write(f"{x:16.8f}{y:16.8f}{z:16.8f}\n")
 
     f.write("PRIMCOORD\n")
@@ -128,180 +142,225 @@ def writexsf(f, a, c, l, v, mode="crystal"):
         f.write(f"{atomsymtonum[l[i]]:4d}{x:16.8f}{y:16.8f}{z:16.8f}\n")
 
 
-def cartesiantocrystal(a, v):
+def cart2crys(a, v):
     """
-        a[i, x]: unitcell
-        v[n, x]: atomic coordinates in cartesian basis
+    Args:
+        a[i, x]: (3, 3) array_like
+            Unitcell
+        v[n, x]: (N, 3) array_like
+            Atomic positions in cartesian coordinate
 
-        b[x, i]: reciprocal unitcell
+    Returns:
+        w[n, i]: (N, 3) array_like
+            Atomic positions in crystal coordinate
 
-        n: atomic index
-        i: crystal coordinate index
-        x: cartesian coordinate index
-
-        w[n, i]: atomic coordinates in crystal basis
+    See also:
+        b[x, i]: (3, 3) array_like
+            Reciprocal unitcell
         w[n, i] = sum_x v[n, i]*b[x, i]
+
+        n: atom index
+        i: crystal index
+        x: cartesian index
     """
     a = np.array(a)
     v = np.array(v)
-    b = getreciprocal(a)
-
+    b = getReciprocal(a)
     w = np.dot(v, b)
 
     return w
 
 
-def crystaltocartesian(a, w):
+def crys2cart(a, w):
     """
-        a[i, x]: unitcell
-        w[n, i]: atomic coordinates in crystal basis
+    Args:
+        a[i, x]: (3, 3) array_like
+            Unitcell
+        w[n, i]: (N, 3) array_like
+            Atomic positions in crystal coordinate
 
-        n: atomic index
-        i: crystal coordinate index
-        x: cartesian coordinate index
+    Returns:
+        v[n, x]: (N, 3) array_like
+            Atomic positions in cartesian coordinate
 
-        v[n, x]: atomic coordinates in cartesian basis
+    See also:
         v[n, x] = sum_i w[n, i]*a[i, x]
+
+        n: atom index
+        i: crystal index
+        x: cartesian index
     """
     a = np.array(a)
     w = np.array(w)
-
     v = np.dot(w, a)
 
     return v
 
 
-def getsurface(a, g):
+def getSurfaceCell(a, g):
     """
-        a: unitcell in cartesian coordinate
-        g: miller index defining the surface
-        s: supercell in cartesian coordinate
+    Args:
+        a[i, x]: (3, 3) array_like
+            Cell
+        g: (3) array_like
+            Miller index
+    Returns:
+        asurf[i, x]: (3, 3) array_like
+            Surface cell
     """
     a = np.array(a)
     g = np.array(g, dtype=np.int32)
     g = g//np.gcd.reduce(g)
-    s = np.zeros((3, 3))
+
+    asurf = np.zeros((3, 3))
 
     if np.linalg.det(a) < 0: raise ValueError("det(a) < 0")
 
-    b = getreciprocal(a)
-
-    G = np.dot(b, g)
+    b = getReciprocal(a)
 
     if g[0] == 0 and g[1] == 0 and g[2] == 0:
         raise ValueError
 
     elif g[0] == 0 and g[1] == 0:
-        s[0, :] = a[0, :]
-        s[1, :] = a[1, :]
-        s[2, :] = a[2, :]
+        asurf[0, :] = a[0, :]
+        asurf[1, :] = a[1, :]
+        asurf[2, :] = a[2, :]
 
     elif g[1] == 0 and g[2] == 0:
-        s[0, :] = a[1, :]
-        s[1, :] = a[2, :]
-        s[2, :] = a[0, :]
+        asurf[0, :] = a[1, :]
+        asurf[1, :] = a[2, :]
+        asurf[2, :] = a[0, :]
 
     elif g[2] == 0 and g[0] == 0:
-        s[0, :] = a[2, :]
-        s[1, :] = a[0, :]
-        s[2, :] = a[1, :]
+        asurf[0, :] = a[2, :]
+        asurf[1, :] = a[0, :]
+        asurf[2, :] = a[1, :]
 
     elif g[0] == 0:
-        s[0, :] = a[0, :]
-        s[1, :] = (a[1, :]/g[1] - a[2, :]/g[2])*np.lcm(g[1], g[2])
+        asurf[0, :] = a[0, :]
+        asurf[1, :] = (a[1, :]/g[1] - a[2, :]/g[2])*np.lcm(g[1], g[2])
 
         if np.abs(g[1])/np.linalg.norm(a[1, :]) > np.abs(g[2])/np.linalg.norm(a[2, :]):
-            s[2, :] = a[1, :]
+            asurf[2, :] = a[1, :]
         else:
-            s[2, :] = a[2, :]
+            asurf[2, :] = a[2, :]
 
     elif g[1] == 0:
-        s[0, :] = a[1, :]
-        s[1, :] = (a[2, :]/g[2] - a[0, :]/g[0])*np.lcm(g[2], g[0])
+        asurf[0, :] = a[1, :]
+        asurf[1, :] = (a[2, :]/g[2] - a[0, :]/g[0])*np.lcm(g[2], g[0])
 
         if np.abs(g[2])/np.linalg.norm(a[2, :]) > np.abs(g[0])/np.linalg.norm(a[0, :]):
-            s[2, :] = a[2, :]
+            asurf[2, :] = a[2, :]
         else:
-            s[2, :] = a[0, :]
+            asurf[2, :] = a[0, :]
 
     elif g[2] == 0:
-        s[0, :] = a[2, :]
-        s[1, :] = (a[0, :]/g[0] - a[1, :]/g[1])*np.lcm(g[0], g[1])
+        asurf[0, :] = a[2, :]
+        asurf[1, :] = (a[0, :]/g[0] - a[1, :]/g[1])*np.lcm(g[0], g[1])
 
         if np.abs(g[0])/np.linalg.norm(a[0, :]) > np.abs(g[1])/np.linalg.norm(a[1, :]):
-            s[2, :] = a[0, :]
+            asurf[2, :] = a[0, :]
         else:
-            s[2, :] = a[1, :]
+            asurf[2, :] = a[1, :]
 
     else:
-        s[0, :] = (a[0, :]/g[0] - a[1, :]/g[1])*np.lcm(g[0], g[1])
-        s[1, :] = (a[1, :]/g[1] - a[2, :]/g[2])*np.lcm(g[1], g[2])
+        asurf[0, :] = (a[0, :]/g[0] - a[1, :]/g[1])*np.lcm(g[0], g[1])
+        asurf[1, :] = (a[1, :]/g[1] - a[2, :]/g[2])*np.lcm(g[1], g[2])
 
         x = [np.abs(g[i])/np.linalg.norm(a[i, :]) for i in range(3)]
-        s[2, :] = a[np.argmax(x), :]
+        asurf[2, :] = a[np.argmax(x), :]
 
-    if np.linalg.det(s) < 0: raise ValueError("det(s) < 0")
+    if np.linalg.det(asurf) < 0: raise ValueError("det(asurf) < 0")
 
-    return G, s
+    return asurf
 
 
-def expandcell(a, p, l, w, ns=1):
+def collectAtom(a, p, l, v, ns=1):
     """
-    expand unitcell to a much larger cell containing
-    a[i, x]: unitcell
-    p[j, i]: coefficients of new unitcell in terms of unitcell
-    l[n]   : labels of atoms within a[i, x]
-    w[n, x]: atomic coordinates within a[i, x]
-    L[n]   : labels of atoms within larger cell
-    W[n, x]: atomic coordinates within larger cell
     """
+    p1 = np.amin(p, axis=0).astype(int) - ns
+    p2 = np.amax(p, axis=0).astype(int) + ns
 
-    """
-    find all the atoms that are contained in the box formed
-    by the following six vertices:
-    1. min(p[:, 0])*a[0, :]
-    2. min(p[:, 1])*a[1, :]
-    3. min(p[:, 2])*a[2, :]
-    4. max(p[:, 0])*a[0, :]
-    5. max(p[:, 2])*a[1, :]
-    6. max(p[:, 2])*a[2, :]
-    """
-
-    p1 = np.amin(p, axis=0).astype(int)
-    p2 = np.amax(p, axis=0).astype(int)
-
-    W = []
     L = []
+    V = []
 
-    for i in range(p1[0]-ns, p2[0]+ns):
-        for j in range(p1[1]-ns, p2[1]+ns):
-            for k in range(p1[2]-ns, p2[2]+ns):
-                W += (w + np.array([i, j, k], dtype=float)).tolist()
+    for i in range(p1[0], p2[0]):
+        for j in range(p1[1], p2[1]):
+            for k in range(p1[2], p2[2]):
                 L += l
+                V += (v + i*a[0, :] + j*a[1, :] + k*a[2, :]).tolist()
 
-    W = np.array(W)
+    V = np.array(V)
+    return L, V
 
-    return L, W
 
 
-def shrinkcell(s, p, L, W):
-    w = []
-    l = []
-
+def reduceAtom(a, p, L, V):
+    """
+    """
     p1 = np.amin(p, axis=0).astype(float)
     p2 = np.amax(p, axis=0).astype(float)
 
-    for li, wi in list(zip(L, W)):
-        mask = np.isclose(wi, p1)
-        wi_clip = np.array([0.0 if mask[x] else wi[x] for x in range(3)])
+    l = []
+    v = []
 
-        if all(p1 <= wi_clip) and all(wi_clip < p2):
-            w.append(wi_clip)
-            l.append(li)
+    W = cart2crys(a, V)
 
-    w = np.array(w)
+    for Li, Wi, Vi in list(zip(L, W, V)):
+        mask = np.isclose(Wi, p1)
+        Wi_ = np.array([p1[x] if mask[x] else Wi[x] for x in range(3)])
 
-    return l, w
+        if all(p1 <= Wi_) and all(Wi_ < p2):
+            l.append(Li)
+            v.append(Vi)
+
+    v = np.array(v)
+    return l, v
+
+
+def convertCell(X1, a2):
+    """
+    """
+    a1, l1, v1 = X1
+    b1 = getReciprocal(a1)
+    p21 = np.dot(a2, b1)
+    L, V = collectAtom(a1, p21, l1, v1)
+    p = [[0.0, 0.0, 0.0], [1.0, 1.0, 1.0]]
+    l2, v2 = reduceAtom(a2, p, L, V)
+    return a2, l2, v2
+
+
+def transformCoordinate(X1, a2):
+    """
+    """
+    a1, l1, v1 = X1
+    l2 = l1
+    r12 = np.dot(np.linalg.inv(a1), a2)
+    v2 = np.dot(v1, r12)
+    return a2, l2, v2
+
+
+def changeBoundary(X, p):
+    """
+    """
+    a, l, v = X
+    L, V = collectAtom(a, p, l, v)
+    l, v = reduceAtom(a, p, L, V)
+    return a, l, v
+
+
+def makeSlab(X, wmin, wmax, woff, dgap):
+    """
+    """
+    p = np.array([[0, 0, wmin], [1, 1, wmax]])
+    t = np.array([0, 0, woff])
+    a, l, v = changeBoundary(X, p+t)
+    w = cart2crys(a, v)
+    w -= t
+    v = crys2cart(a, w)
+    dsurf = a[2, 2]
+    a[2, :]  = np.array([0.0, 0.0, (wmax - wmin)*dsurf + dgap])
+    return a, l, v
 
 
 if __name__ == "__main__":
@@ -310,120 +369,44 @@ if __name__ == "__main__":
         "float": lambda x: "{:15.8f}".format(x),
         "int": lambda x: "{:4d}".format(x)})
 
-    # building a slab of Bi2Se3 from scratch (bulk structure)
-
-    # A0: primitive unitcell 
-    # W0: atomic coordinates within A0 in crystal coordinates
-    # V0: atomic coordinates within A0 in cartesian coordinates
-    # L0: atom labels within A0
+    # Build a 3QL slab of Bi2Se3
     import Bi2Se3
 
-    A0 = Bi2Se3.rhombo(9.841, 24.27*np.pi/180.0)
+    aprim = Bi2Se3.rhombo(9.841, 24.27*np.pi/180.0)
     u1, u2 = (0.4, 0.21)
 
-    W0 = np.zeros((5, 3))
-    W0[0, :] = np.array([0.0, 0.0, 0.0])
-    W0[1, :] = np.array([u2, u2, u2])
-    W0[2, :] = -W0[1, :]
-    W0[3, :] = np.array([u1, u1, u1])
-    W0[4, :] = -W0[3, :]
-    W0 = np.mod(np.array(W0), [1.0, 1.0, 1.0])
+    wprim = np.array([
+        [0.0, 0.0, 0.0],
+        [ u2,  u2,  u2],
+        [-u2, -u2, -u2],
+        [ u1,  u1,  u1],
+        [-u1, -u1, -u1]
+    ], dtype=float)
 
-    V0 = crystaltocartesian(A0, W0)
-    L0 = ["Se", "Se", "Se", "Bi", "Bi"]
+    wprim = np.mod(wprim, [1.0, 1.0, 1.0])
 
-    """
-    G[x]: sum_i b[x, i]*h[i]
-    s[i, x]: new unitcell for generating a surface perpendicular G[x]
-    """
-    # gs : miller index of the surface normal vector of a slab to be created
-    # Gs : surface normal vector in cartesian coordinates
-    # A0s: new unitcell for creation of a slab model
-    gs = [1, 1, 1]
-    Gs, A0s = getsurface(A0, gs)
+    vprim = crys2cart(aprim, wprim)
+    lprim = ["Se", "Se", "Se", "Bi", "Bi"]
 
-    B0 = getreciprocal(A0)
-    box = np.dot(A0s, B0)
+    # Unitcell
+    Xprim = (aprim, lprim, vprim)
 
-    # W1: 
-    # V1: 
-    # L1: 
-    L1, W1 = expandcell(A0, box, L0, W0, ns=1)
-    V1 = crystaltocartesian(A0, W1)
+    # Convert to surface cell
+    gsurf = [1, 1, 1]
+    Gsurf = getGvector(aprim, gsurf)
+    asurf = getSurfaceCell(aprim, gsurf)
+    Xsurf = convertCell(Xprim, asurf)
 
+    # Coordinate transformation
+    Xsurf = transformCoordinate(Xsurf, standardizeCell(asurf))
 
-    """
-    remove all the atoms that are located outside the unitcell
-    previously obtained
-    """
-    # W1s:
-    # V1s:
-    # L1s:
-    W1s = cartesiantocrystal(A0s, V1)
-    L1s = L1
+    # Make slab
+    wmin, wmax, woff, gap = -1.0, 1.0, 0.5, 20.0
+    Xslab = makeSlab(Xsurf, wmin, wmax, woff, gap)
 
-    box = [[0.0, 0.0, 0.0],
-           [1.0, 1.0, 1.0]]
-
-    L0s, W0s = shrinkcell(A0s, box, L1s, W1s)
-    V0s = crystaltocartesian(A0s, W0s)
-
-
-    """
-    transformation of the coordinate axes
-    A0s[0, :] --> A0t[0, :] on xy plane, parallel to x
-    A0s[1, :] --> A0t[1, :] on xy plane
-    A0s[2, :] --> A0t[2, :] parallel to z (automatically)
-    """
-    A0t = celldmtocell(celltocelldm(A0s))
-
-    """
-    coordinate transformation matrix
-    Rst[x, x']  = sum_i A0s^(-1)[x, i]*A0t[i, x']
-    V0t[i, x'] = sum_x V0s[i, x]*Rst[x, x']
-    """
-    Rst = np.dot(np.linalg.inv(A0s), A0t)
-    V0t = np.dot(V0s, Rst)
-    W0t = cartesiantocrystal(A0t, V0t)
-    L0t = L0s
-
-
-    """
-    set window parameters (along the z' direction)
-    """
-    wmin, wmax, wtrans, gap = -1.5, 1.5, 0.0, 10.0
-
-    """
-    find all the atoms within the slab of width (winmax - winmax)/|G|
-    note that 1/|G| is the width between adjacent [h1, h2, h3] planes
-    """
-    box = [[0, 0, np.floor(wmin)],
-           [1, 1, np.ceil(wmax)]]
-    L1t, W1t = expandcell(A0t, box, L0t, W0t, ns=0)
-
-
-    """
-    remove all the atoms outside the window and apply (if given) a translation
-    """
-    box = [[0.0, 0.0, wmin],
-           [1.0, 1.0, wmax]]
-    L0t, W0t = shrinkcell(A0t, box, L1t, W1t)
-    V0t = crystaltocartesian(A0t, W0t)
-
-    A0t[2, :] *= wmax - wmin
-
-
-    """
-    finally, insert a vacuum layer in the z' direction and set s'[2, :]
-    to be parallel to z' direction
-    """
-    A0t[2, :] = np.array([0.0, 0.0, (wmax - wmin)/np.linalg.norm(Gs)])
-    A0t[2, :] += np.array([0.0, 0.0, gap])
-
-    with open("/tmp/tmp.xsf", 'w') as f:
-        writexsf(f, A0t, A0t, L0t, V0t, mode="slab")
-
+    with open("tmp.xsf", 'w') as f:
+        writexsf(f, *Xslab, mode="slab")
     import subprocess
-    subprocess.call(["/home/kimtaeyun/xcrysden/1.6.2/xcrysden", "--xsf", "/tmp/tmp.xsf"])
+    subprocess.call(["/home/kimtaeyun/xcrysden/1.6.2/xcrysden", "--xsf", "tmp.xsf"])
     subprocess.call(["rm", "-f", "/tmp/tmp.xsf"])
 
